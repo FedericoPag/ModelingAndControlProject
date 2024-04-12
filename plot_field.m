9cf1a61d7743442c77dde2a5e7a1d447a606b012
FedericoPag <s282862@studenti.polito.it>
2024-04-12 18:35:09 +0200

    Something useless has been removed

--- plot_field.m
+++ plot_field.m
@@ -1,23 +1,15 @@
-function plot_field(n, H, L, M, n_iter)
-    
+function plot_field(n, H, L, M, n_iter)    
     W = H*L;
     room_grid = zeros(2,n);
-
 
     for i=1:n
 	    room_grid(1,i) = floor(W/2)+ mod(i-1,L)*W; 
 	    room_grid(2,i) = floor(W/2)+ floor((i-1)/L)*W;
     end
-    
-    xtrue = zeros(n,1);
-    support = randperm(n);
-    support = support(1:3); % I consider 3 targets
-    xtrue(support) = 1;
-    target = find(xtrue);
 
     for move = 1:n_iter
-        xtrue = M(1:n,move);
-        target = find(xtrue);
+        x_obtained = M(1:n,move);
+        target = find(x_obtained);
     
         plot(room_grid(1,target), room_grid(2,target),'s','MarkerSize',9, 'MarkerEdgeColor',1/255*[40 208 220],'MarkerFaceColor',1/255*[40 208 220])
         grid on
