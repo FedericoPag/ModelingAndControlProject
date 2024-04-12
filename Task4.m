52759a71bae192181e4c5ccb3615e2c3aa2ea10b
FedericoPag <s282862@studenti.polito.it>
2024-04-12 18:28:58 +0200

    Modified Task 4, removing ISTA and loading z_new from file z_new_task_3.mat

--- Task4.m
+++ Task4.m
@@ -9,38 +9,16 @@ eps = 1e-8;
 delta = 1e-12;
 debug = 0;
 
-load("localization.mat")
+load("tracking_moving_targets.mat");
+load("z_new_task_3.mat");
 
 G = normalize([D eye(q)]);
 tau = norm(G)^(-2) - eps;
 lambda = [10 20];
 Gamma = tau * [lambda(1)*ones(p, 1); lambda(2)*ones(q, 1)];
-z = zeros(q+p,1);
 
-%% ISTA
-while 1
-    z_new = thresholding(z + tau*G'*( y - G*z ) , Gamma);
-    norm_difference_squared = norm(z_new - z);
-    z = z_new;
-    if norm_difference_squared < delta
-        break
-    end
-end
-
-% Zerofying numbers under a threshold (tol)
-tol = 4;
-necessary = 1;
-
-if necessary == 1
-    for i=1:(p+q)
-       if abs(z_new(i)) < tol
-            z_new(i)=0;
-        end
-    end
-end
 
 %% Sparse observer
-load("tracking_moving_targets.mat");
 z_hat = zeros(p+q,1);
 n_iter = 50;
 
